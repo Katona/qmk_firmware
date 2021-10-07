8d170d03583a2281db8f0cb69fbd789130ec8d43
Gabor Hornyak <gabhornyak@gmail.com>
2021-10-07 18:39:08 +0200

    feat: enable slave/master based on USB

--- keyboards/lily58/keymaps/katona/config.h
+++ keyboards/lily58/keymaps/katona/config.h
@@ -22,9 +22,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
-// #define EE_HANDS
+#define EE_HANDS
+#define SPLIT_USB_DETECT
 
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 300
